09d10b8e477af883277f9e2e7eed254b4794d187
admin <admin@H2osus-mac.local>
2023-11-11 18:07:14 +0200

    single service content

--- template-parts/static-blocks/one-service.php
+++ template-parts/static-blocks/one-service.php
@@ -74,7 +74,7 @@
             <p class="one-service__info-graduate--text"><?= esc_html($wordChar); ?> <span>(<?= esc_html($countGrade) ?> <?= esc_html__('оценок', 'cloud_miners') ?>)</span></p>
             <a target="_blank" href="#" rel=”nofollow” class="button telegram-button">
                 <img src="<?= get_template_directory_uri() . '/src/img/images/svg/telegram.svg'?>" alt="Author"/>
-                Telegram
+                <?= esc_html__('Telegram', 'cloud_miners') ?>
             </a>
         </div>
     </div>
@@ -116,127 +116,7 @@
     </div>
 
     <div class="one-service__content">
-        <p>Лучшей системой, которая позволит каждому желающему получать криптовалюту без внушительных вложений фиатных средств, является сервис BitMine. По сравнению с другими проектами, этот ресурс выглядит очень перспективно. Развивается он стабильно и платить хорошие деньги всем пользователям. Уровень доходности в этом проекте составляет больше 100%, то есть можно сказать, что за полгода пользователь может увеличить сумму Биткоинов примерно в два раза.</p>
-        <div class="service-navigation" id="service-navigation">
-            <div class="open-navigation" id="open-navigation">
-                <div class="open-navigation__left">
-                    <img src="<?= get_template_directory_uri() . '/src/img/images/svg/nav.svg'?>" alt="" />
-                    <p>Навигация</p>
-                </div>
-                <img src="<?= get_template_directory_uri() . '/src/img/images/svg/arrow-small.svg'?>" alt="" class="open-navigation__arrow"/>
-            </div>
-            <ul class="list-bordered">
-                <li><a href="id1">Обзор сервиса облачного майнинга BitMiner</a></li>
-                <li><a href="id2">О проэкте</a></li>
-                <li><a href="id3">Видео обзоры</a></li>
-                <li><a>Реферальная программа</a></li>
-                <li><a>Вывод криптовалюты на кошелек</a></li>
-                <li><a>Выводы</a></li>
-                <li><a>Отзывы</a></li>
-            </ul>
-        </div>
-
-        <h3 id="id1">Обзор сервиса облачного майнинга BitMiner</h3>
-        <p>Данный проект является довольно востребованным среди пользователей. Он отличается удобным интерфейсом
-            и позволяет майнить достаточное количество альткоинов.</p>
-
-        <div class="advantages">
-            <h4>Преимущества</h4>
-            <ul>
-                <li>Возможность открытия депозита. Все заработанные деньги можно в любой момент.</li>
-                <li>Статус майнинга. Во вкладке «майнинг» можно увидеть статусы всех вычислений и новые блоки.</li>
-                <li>Возможность открытия депозита. Все заработанные деньги можно в любой момент.</li>
-                <li>Статус майнинга. Во вкладке «майнинг» можно увидеть статусы всех вычислений и новые блоки.</li>
-            </ul>
-        </div>
-
-        <div class="flaws">
-            <h4>Недостатки</h4>
-            <ul>
-                <li>Возможность открытия депозита. Все заработанные деньги можно в любой момент.</li>
-                <li>Статус майнинга. Во вкладке «майнинг» можно увидеть статусы всех вычислений и новые блоки.</li>
-            </ul>
-        </div>
-
-        <div class="note">
-            <p>Выделенный текст. Fermentum tincidunt sagittis sapien, facilisi congue non fringilla sit. Est proin adipiscing neque, at est tincidunt a sollicitudin. Ipsum quis sapien iaculis blandit. Urna egestas nam quisque at mattis. Aliquet netus ac, ut ultrices pellentesque. Sed ullamcorper.</p>
-        </div>
-
-        <h3 id="id3">О проекте</h3>
-        <p>Данный проект является довольно востребованным среди пользователей. Он отличается удобным интерфейсом и позволяет майнить достаточное количество альткоинов.</p>
-        <h4>Стоит ли инвестировать в проект?</h4>
-        <p>Ответ однозначный – нужно. Но делать это рекомендуется пользователям, которые уже используют другие программы для добычи монет. Возможность зарабатывать деньги пассивно выпадает не так часто. Сегодня благодаря возможности заниматься майнингом криптовалюты каждый может зарабатывать высокие суммы денег без каких-либо усилий.</p>
-        <p>Возможность зарабатывать деньги пассивно выпадает не так часто. Сегодня благодаря возможности заниматься майнингом криптовалюты:</p>
-        <ul>
-            <li>Ежедневно количество монет на счету увеличивается;</li>
-            <li>Используется дополнительный уровень мощности;</li>
-            <li>Возможность открыть депозит с партнером проекта;</li>
-            <li>Присутствие реферальной программы.</li>
-        </ul>
-
-        <h3  id="id2">Видео обзоры</h3>
-        <p>Одним из преимуществ проекта является договоренность с сервисом, который каждому пользователю предоставляется возможность размещения всех денег на депозит под 1% в месяц. Это достаточно хорошая функция, которая позволяет дополнительно увеличить уровень дохода без особых усилий.</p>
-        <div class="video-block">
-            <div class="video-block__show">
-                <img src="<?= get_template_directory_uri() . '/src/img/images/svg/play.svg'?>" alt="look" class="video-block__show-icon"/>
-                <img src="<?= get_template_directory_uri() . '/src/img/images/video.png'?>" alt="Image" class="video-block__image"/>
-            </div>
-            <div class="video-block__show">
-                <img src="<?= get_template_directory_uri() . '/src/img/images/svg/play.svg'?>" alt="look" class="video-block__show-icon"/>
-                <img src="<?= get_template_directory_uri() . '/src/img/images/video.png'?>" alt="Image" class="video-block__image"/>
-            </div>
-        </div>
-
-        <h3>Реферальная программа</h3>
-        <p>На сервисе, конечно, же есть реферальная программа. Как и в многих подобных проектах сюда также можно приглашать неограниченное количество пользователей, что принесет на счет реферала дополнительные средства за каждого нового участника.</p>
-        <p>Представим, что человек пригласил одного пользователя и тот прошел успешную регистрацию на сервисе, а затем пополнил свой счет на 5 долларов. Такие действия принесут вам уровень мощности для майнинга в размере 1 Gh/s. Если пользователь пригласил 100 человек и все они зарегистрировались и внесли по 5 долларов, то его уровень мощности майнинга таким образом увеличится на 1001 Gh/s. Хорошо, не правда ли? Приглашая других людей по реферальной программе, вы не только дарите им возможность зарабатывать пассивно без высоких вложений, но и сами получаете весомую прибыль.</p>
-        <div class="quote">
-            <p>Выделенный коментарий/цитата. Fermentum tincidunt sagittis sapien, facilisi congue non fringilla sit. Est proin adipiscing neque, at est tincidunt a sollicitudin. Ipsum quis sapien iaculis blandit. Urna egestas nam quisque at mattis. Aliquet netus ac, ut ultrices pellentesque. Sed ullamcorper.   <a href="#">Перейти к отзыву</a></p>
-            <div>
-                <p>Андрей Иванов</p>
-                <p>29.04.2023</p>
-            </div>
-        </div>
-
-
-        <h4>Работа сервиса</h4>
-        <p>Программа BitMine имеет следующие характеристики:</p>
-        <ol>
-            <li>Возможность открытия депозита. Все заработанные деньги можно в любой момент положить на выгодный депозит, которые предлагает партнер проекта Vault Interest. Не многие сервисы способны похвастаться такими возможностями.</li>
-            <li>Уровень цены на единицу мощности майнинга (1 Gh/s) составляет примерно 0,0007 биткоина (это вполне разумный показатель).</li>
-            <li>Для начала работы необходимо купить минимум 1 Gh/s. Покупку мощности для майнинга можно проводить неограниченное число раз. В других проектах облачного майнинга такой возможности нет, как правило, устанавливается ограничение.</li>
-            <li>Статус майнинга. Во вкладке «майнинг» можно увидеть статусы всех вычислений и новые блоки.</li>
-            <li>Покупка асиков. Чтобы приобрести асики, больше не нужно переходить на другие сайты, которые требуют обязательную регистрацию.</li>
-        </ol>
-
-        <a href="#">
-            <img src="<?= get_template_directory_uri() . '/src/img/images/svg/notifications.svg'?>" alt="look"/>
-            Ссылка на новость, которая относится по тематике
-        </a>
-
-        <h3>Вывод криптовалюты на кошелек</h3>
-        <p>Одним из преимуществ проекта является договоренность с сервисом, который каждому пользователю предоставляется возможность размещения всех денег на депозит под 1% в месяц. Это достаточно хорошая функция, которая позволяет дополнительно увеличить уровень дохода без особых усилий.</p>
-        <div class="img-block">
-            <a href="#" class="img-block__show" data-lightbox="roadtrip">
-                <img src="<?= get_template_directory_uri() . '/src/img/images/svg/search-show.svg'?>" alt="look" class="img-block__show-icon"/>
-                <img src="<?= get_template_directory_uri() . '/src/img/images/one-service.png'?>" alt="Image" class="img-block__image"/>
-            </a>
-            <a href="#" class="img-block__show" data-lightbox="roadtrip">
-                <img src="<?= get_template_directory_uri() . '/src/img/images/svg/search-show.svg'?>" alt="look" class="img-block__show-icon"/>
-                <img src="<?= get_template_directory_uri() . '/src/img/images/one-service.png'?>" alt="Image" class="img-block__image"/>
-            </a>
-            <a href="#" class="img-block__show" data-lightbox="roadtrip">
-                <img src="<?= get_template_directory_uri() . '/src/img/images/svg/search-show.svg'?>" alt="look" class="img-block__show-icon"/>
-                <img src="<?= get_template_directory_uri() . '/src/img/images/one-service.png'?>" alt="Image" class="img-block__image"/>
-            </a>
-            <a href="#" class="img-block__show" data-lightbox="roadtrip">
-                <img src="<?= get_template_directory_uri() . '/src/img/images/svg/search-show.svg'?>" alt="look" class="img-block__show-icon"/>
-                <img src="<?= get_template_directory_uri() . '/src/img/images/one-service.png'?>" alt="Image" class="img-block__image"/>
-            </a>
-        </div>
-
-        <h3>Выводы</h3>
-        <p>Одним из преимуществ проекта является договоренность с сервисом, который каждому пользователю предоставляется возможность размещения всех денег на депозит под 1% в месяц. Это достаточно хорошая функция, которая позволяет дополнительно увеличить уровень дохода без особых усилий.</p>
+        <?= the_content() ?>
         <div class="sharing">
             <div class="sharing__share">
                 <p>Поделиться</p>
