a48c4b5207377f796d9d4b8e2b3798ffb39cbb62
admin <admin@H2osus-mac.local>
2023-12-15 00:50:51 +0200

    one more last

--- functions.php
+++ functions.php
@@ -490,6 +490,24 @@ add_action( 'wp_footer', function() {
 
 //
 
+//    add_filter('author_rewrite_rules', 'no_author_base_rewrite_rules');
+//    function no_author_base_rewrite_rules($author_rewrite) {
+//        global $wpdb;
+//        $author_rewrite = array();
+//        $authors = $wpdb->get_results("SELECT user_nicename AS nicename from $wpdb->users");
+//        foreach($authors as $author) {
+//            $author_rewrite["({$author->nicename})/page/?([0-9]+)/?$"] = 'index.php?author_name=$matches[1]&paged=$matches[2]';
+//            $author_rewrite["({$author->nicename})/?$"] = 'index.php?author_name=$matches[1]';
+//        }
+//        return $author_rewrite;
+//    }
+//    add_filter('author_link', 'no_author_base', 1000, 2);
+//    function no_author_base($link, $author_id) {
+//        $link_base = trailingslashit(get_option('home'));
+//        $link = preg_replace("|^{$link_base}author/|", '', $link);
+//        return $link_base . $link;
+//    }
+
     add_filter('author_rewrite_rules', 'no_author_base_rewrite_rules');
     function no_author_base_rewrite_rules($author_rewrite) {
         global $wpdb;
@@ -501,10 +519,13 @@ add_action( 'wp_footer', function() {
         }
         return $author_rewrite;
     }
-    add_filter('author_link', 'no_author_base', 1000, 2);
-    function no_author_base($link, $author_id) {
-        $link_base = trailingslashit(get_option('home'));
-        $link = preg_replace("|^{$link_base}author/|", '', $link);
-        return $link_base . $link;
+
+    if( !is_admin() ) {
+        add_action('init', 'author_rewrite_so_22115103');
+    }
+
+    function author_rewrite_so_22115103() {
+        global $wp_rewrite;
+        if( 'author' == $wp_rewrite->author_base ) $wp_rewrite->author_base = null;
     }
     ?>
