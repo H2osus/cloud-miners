619bde4e1641eeafdb5a615f38bc08dbcf759ea2
admin <admin@H2osus-mac.local>
2023-12-18 18:33:33 +0200

    fix author page

--- template-parts/static-blocks/breadcrumbs.php
+++ template-parts/static-blocks/breadcrumbs.php
@@ -1,4 +1,4 @@
-<?php //if (!is_author()): ?>
+<?php if (!is_author()): ?>
 <div class="breadcrumbs">
     <img src="<?= get_template_directory_uri() . '/src/img/images/breadcrumbs.png'?>" alt="banner mask" class="decoration"/>
     <div class="container">
@@ -8,20 +8,21 @@
     </div>
 </div>
 
-<?php //endif; ?>
+<?php endif; ?>
 
-<?php //if (is_author() && isset($_GET['b'])):
-//    $postType = 'articles';
-//    $term = wp_get_post_terms($_GET['b'], 'category-articles'); ?>
-<!--    <div class="breadcrumbs">-->
-<!--        <img src="--><?php //= get_template_directory_uri() . '/src/img/images/breadcrumbs.png'?><!--" alt="banner mask" class="decoration"/>-->
-<!--        <div class="container">-->
-<!--            <p id="breadcrumbs" class="breadcrumbs-content">-->
-<!--                <span>-->
-<!--                    <span>-->
-<!--                        <a href="--><?php //= home_url('/') ?><!--">--><?php //= esc_html__('Главная', 'cloud_miners') ?><!--</a>-->
-<!--                    </span>-->
-<!--                    /-->
+<?php if (is_author()): // && isset($_GET['b'])
+    $postType = 'articles';
+//    $term = wp_get_post_terms($_GET['b'], 'category-articles');
+    ?>
+    <div class="breadcrumbs">
+        <img src="<?= get_template_directory_uri() . '/src/img/images/breadcrumbs.png'?>" alt="banner mask" class="decoration"/>
+        <div class="container">
+            <p id="breadcrumbs" class="breadcrumbs-content">
+                <span>
+                    <span>
+                        <a href="<?= home_url('/') ?>"><?= esc_html__('Главная', 'cloud_miners') ?></a>
+                    </span>
+                    /
 <!--                    <span>-->
 <!--                        <a href="--><?php //= get_post_type_archive_link($postType) ?><!--">--><?php //= esc_html__('Статьи','cloud_miners') ?><!--</a>-->
 <!--                    </span>-->
@@ -36,11 +37,14 @@
 <!--                        <a href="--><?php //= get_permalink($_GET['b']) ?><!--">--><?php //= get_the_title($_GET['b'])?><!--</a>-->
 <!--                    </span>-->
 <!--                    /-->
-<!--                    <span class="breadcrumb_last" aria-current="page">admin</span>-->
-<!--                </span>-->
-<!--            </p>-->
-<!--        </div>-->
-<!--    </div>-->
-<?php //endif; ?>
+                    <?php $authorID = get_post_field( 'post_author', get_the_ID() );
+                    $niceName = get_the_author_meta( 'display_name', $authorID );
+                    ?>
+                    <span class="breadcrumb_last" aria-current="page"><?= esc_html($niceName) ?></span>
+                </span>
+            </p>
+        </div>
+    </div>
+<?php endif; ?>
 
 
